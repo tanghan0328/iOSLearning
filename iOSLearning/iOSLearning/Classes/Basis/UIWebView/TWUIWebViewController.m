b2c0a2ef865d0c1c8a7ce1bd685f9d4247680c48
tanghan0328 <tang@tangdeMacBook-Pro.local>
2016-06-07 15:25:43 +0800

    增加了长按保存图片的功能
    增加了长按保存图片的功能

--- iOSLearning/iOSLearning/Classes/Basis/UIWebView/TWUIWebViewController.m
+++ iOSLearning/iOSLearning/Classes/Basis/UIWebView/TWUIWebViewController.m
@@ -5,10 +5,11 @@
 //  Created by tang on 16/6/6.
 //  Copyright © 2016年 tang. All rights reserved.
 //
+//长按将图片存储在手机的照片中
 
 #import "TWUIWebViewController.h"
 
-@interface TWUIWebViewController ()<UIWebViewDelegate>
+@interface TWUIWebViewController ()<UIWebViewDelegate,UIGestureRecognizerDelegate>
 
 @property (strong, nonatomic) IBOutlet UIWebView *webView;
 
@@ -32,10 +33,97 @@
     self.webView.delegate = self;
     self.webView.scalesPageToFit = YES;//自动对页面进行缩放以适应屏幕
 
-    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.baidu.com"]]];
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://photo.gmw.cn/2016-06/07/content_20449736_3.htm"]]];
     
+    UILongPressGestureRecognizer* longPressed = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longPressed:)];
+    longPressed.delegate = self;
+    [self.webView addGestureRecognizer:longPressed];
+
 }
 
+- (void)longPressed:(UILongPressGestureRecognizer*)recognizer
+{
+    if (recognizer.state != UIGestureRecognizerStateBegan) {
+        return;
+    }
+    CGPoint touchPoint = [recognizer locationInView:self.webView];
+    
+    NSString *imgURL = [NSString stringWithFormat:@"document.elementFromPoint(%f, %f).src", touchPoint.x, touchPoint.y];
+    NSString *urlToSave = [self.webView stringByEvaluatingJavaScriptFromString:imgURL];
+    
+    if (urlToSave.length == 0) {
+        return;
+    }
+    
+    [self showImageOptionsWithUrl:urlToSave];
+}
+
+- (void)showImageOptionsWithUrl:(NSString *)imageUrl
+{
+//    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"标题" message:@"这个是UIAlertController的默认样式" preferredStyle:UIAlertControllerStyleAlert];
+//    
+//    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil];
+//    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"好的" style:UIAlertActionStyleDefault handler:nil];
+//    [alertController addAction:cancelAction];
+//    [alertController addAction:okAction];
+//
+//    [self presentViewController:alertController animated:YES completion:nil];
+    
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"保存或删除数据"message:@"删除数据将不可恢复" preferredStyle:UIAlertControllerStyleActionSheet];
+    
+//    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil];
+    UIAlertAction *deleteAction = [UIAlertAction actionWithTitle:@"删除" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *deleteAction){
+        NSLog(@"点击了删除的按钮");
+    }];
+    UIAlertAction *archiveAction = [UIAlertAction actionWithTitle:@"保存" style:UIAlertActionStyleDefault handler:^(UIAlertAction *archiveAction){
+        NSLog(@"点击了保存");
+        [self saveImageToDiskWithUrl:imageUrl];
+
+    }];
+//    [alertController addAction:cancelAction];
+    [alertController addAction:deleteAction];
+    [alertController addAction:archiveAction];
+    
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+
+- (void)saveImageToDiskWithUrl:(NSString *)imageUrl
+{
+    NSURL *url = [NSURL URLWithString:imageUrl];
+    
+    NSURLSessionConfiguration * configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration delegate:self delegateQueue:[NSOperationQueue new]];
+    
+    NSURLRequest *imgRequest = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReturnCacheDataElseLoad timeoutInterval:30.0];
+    
+    NSURLSessionDownloadTask  *task = [session downloadTaskWithRequest:imgRequest completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (error) {
+            return ;
+        }
+        NSData * imageData = [NSData dataWithContentsOfURL:location];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            UIImage * image = [UIImage imageWithData:imageData];
+            UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), NULL);
+        });
+    }];
+    [task resume];
+}
+
+- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
+{
+    UIAlertController *alertController = nil;
+    if (error) {
+        alertController = [UIAlertController alertControllerWithTitle:@"标题" message:@"保存失败" preferredStyle:UIAlertControllerStyleAlert];
+    }else{
+        alertController = [UIAlertController alertControllerWithTitle:@"" message:@"保存成功" preferredStyle:UIAlertControllerStyleAlert];
+    }
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleCancel handler:nil];
+    [alertController addAction:cancelAction];
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+
+#pragma mark -- webviewDelegate
 // 开始发送请求（加载数据）时调用这个方法
 - (void)webViewDidStartLoad:(UIWebView *)webView
 {
